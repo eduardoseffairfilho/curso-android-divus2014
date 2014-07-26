9b98b809d744d716907ef9af9977dc0d39f568ef
eduardoseffairfilho@gmail.com <eduardoseffairfilho@gmail.com@7d593da3-de3a-d9b1-b32f-65dc56102bab>
2014-07-26 19:56:54 +0000

    Adicionando tela cadastro de despesa.

---
+++ modulo1/GC2AppBoaViagem/src/com/divus/academia/android/GC2AppBoaViagem/CadastroDespesaActivity.java
@@ -0,0 +1,14 @@
+package com.divus.academia.android.GC2AppBoaViagem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CadastroDespesaActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cadastro_despesa);
+		
+	}
+}
