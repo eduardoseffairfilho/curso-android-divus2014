94f5462fd4fa9a952abdd6c38e4eb058a5644ec2
eduardoseffairfilho@gmail.com <eduardoseffairfilho@gmail.com@7d593da3-de3a-d9b1-b32f-65dc56102bab>
2014-07-26 19:33:47 +0000

    Adicionando a tela de despesas.

---
+++ modulo1/GC2AppBoaViagem/src/com/divus/academia/android/GC2AppBoaViagem/DespesasActivity.java
@@ -0,0 +1,47 @@
+package com.divus.academia.android.GC2AppBoaViagem;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class DespesasActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_despesas);
+	}
+	
+	public void criarDespesaOnclick(View v) {
+		Log.d("TelaDespesa", "método criarDespesaOnclick");
+		Toast.makeText(this, "método criarDespesaOnclick", Toast.LENGTH_LONG).show();
+	}
+	
+	public void excluirDespesaOnclick(View v) {
+		Log.d("TelaDespesa", "método excluirDespesaOnclick");
+		Toast.makeText(this, "método excluirDespesaOnclick", Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.despesas, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
