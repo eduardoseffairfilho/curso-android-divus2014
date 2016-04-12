f633d5630388e51e379e0bee119e1b61f588c89b
Eduardo Jorge Aucar Seffair Filho <eduardoseffairfilho@gmail.com>
2016-04-12 15:52:44 -0400

    Convertendo o projeto GC1ExemploProcessoAssincrono1 para Android Studio

--- modulo1/GC1ExemploProcessoAssincrono/src/com/example/gc1exemploprocessoassincrono/ImagemListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.gc1exemploprocessoassincrono;
-
-import android.graphics.Bitmap;
-
-public interface ImagemListener {
-	
-	void setBitmap(Bitmap imagem);
-
-}
---
+++ modulo1/GC1ExemploProcessoAssincrono1/app/src/main/java/com/example/gc1exemploprocessoassincrono/ImagemListener.java
@@ -0,0 +1,9 @@
+package com.example.gc1exemploprocessoassincrono;
+
+import android.graphics.Bitmap;
+
+public interface ImagemListener {
+	
+	void setBitmap(Bitmap imagem);
+
+}
