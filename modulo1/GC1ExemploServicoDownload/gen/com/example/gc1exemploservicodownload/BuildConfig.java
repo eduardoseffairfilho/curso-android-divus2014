695dc09a27f6bfff87da14124d8b5cdcc61a244e
Eduardo Jorge Aucar Seffair Filho <eduardoseffairfilho@gmail.com>
2016-04-15 13:25:48 -0400

    Convertendo projeto GC1ExemploServicoDownload para Android Studio

--- modulo1/GC1ExemploServicoDownload/gen/com/example/gc1exemploservicodownload/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.gc1exemploservicodownload;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
